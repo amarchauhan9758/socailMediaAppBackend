98894e0ea6abfe1a8f996d58e6477cad14e679da
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-07-30 14:07:49 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+  });
 };
 
 module.exports = connectDB;
